41bf5615f318bb5b5d44661f30ec37dee124c3b3
Konz <konz@Konzs-MacBook-Air.local>
2023-10-03 11:51:39 +0700

    demo toggle showmore and showless

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { PostDTO } from '../types/dto'
 import classes from './Post.module.css'
 
@@ -6,17 +7,19 @@ interface IPostProps {
 }
 
 const Post = ({ post }: IPostProps) => {
-  const handleClick = (id: number) => () => {
-    alert(`clicked ${id}`)
-  }
+  const [showMore, setShowMore] = useState<boolean>(false)
 
   return (
-    <div onClick={handleClick(post.id)} className={classes.post}>
-      <p>id: {post.id}</p>
-      <p>postedBy: {post.userId}</p>
-      <p>title: {post.title}</p>
-      <p>body: {post.body}</p>
-    </div>
+    <>
+      <div className={classes.post}>
+        <p>id: {post.id}</p>
+        <p>postedBy: {post.userId}</p>
+        <p>title: {post.title}</p>
+        <p>body: {post.body}</p>
+        {showMore && <p>more infomation...</p>}
+        <button onClick={() => setShowMore(!showMore)}>{showMore ? 'Show less' : 'Show More'}</button>
+      </div>
+    </>
   )
 }
 export default Post
