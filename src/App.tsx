ada43dc16efb4ad9ff4361d5ae93ee5bbe10801f
Konz <konz@Konzs-MacBook-Air.local>
2023-10-04 15:23:12 +0700

    init axios && fetch with axios

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Greeting from './components/Greeting'
 import Navbar from './components/Navbar'
 import Post from './components/Post'
 import { PostDTO } from './types/dto'
+import axios from 'axios'
 
 function App() {
   const [posts, setPosts] = useState<PostDTO[] | null>(null)
@@ -15,14 +16,16 @@ function App() {
     const fetchData = async () => {
       setIsLoading(true)
       try {
-        const res = await fetch('https://jsonplaceholder.typicode.com/posts')
-        const data = await res.json()
+        // const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+        // const data = await res.json()
 
-        if (!res.ok) {
-          throw new Error('error')
-        }
+        // if (!res.ok) {
+        //   throw new Error('error')
+        // }
 
-        setPosts(data)
+        const res = await axios.get<PostDTO[]>('https://jsonplaceholder.typicode.com/posts')
+
+        setPosts(res.data)
       } catch (err) {
         console.error(err)
       } finally {
