142a0c2fc688b3974120fc502dcc55e23501c1bc
Konz <konz@Konzs-MacBook-Air.local>
2023-10-05 15:49:09 +0700

    fetch hook post

--- src/pages/PostDetail.tsx
+++ src/pages/PostDetail.tsx
@@ -1,9 +1,21 @@
 import { useParams } from 'react-router-dom'
+import usePost from '../hooks/usePost'
 
 const PostDetail = () => {
   const { id } = useParams()
+  const { posts, isLoading } = usePost(id || '1')
 
-  return <h1>PostDetail id: {id}</h1>
+  console.log(posts)
+
+  if (isLoading) return <h1>Loading...</h1>
+
+  return (
+    <div>
+      <h1>{posts?.id}</h1>
+      <h1>{posts?.title}</h1>
+      <p>{posts?.body}</p>
+    </div>
+  )
 }
 
 export default PostDetail
