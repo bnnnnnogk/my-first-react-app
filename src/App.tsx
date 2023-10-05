3667021054249ad8ce8576e196746a414d6c6daf
Konz <konz@Konzs-MacBook-Air.local>
2023-10-05 10:54:45 +0700

    change code for profile and remove dynamic msg

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import './App.css'
-import Greeting from './components/Greeting'
 import Navbar from './components/Navbar'
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Greeting name="Bun" isLoggedIn={true} />
     </div>
   )
 }
