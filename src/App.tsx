bfac84b96ad931aca878fbf2c5ab6090b724545b
Konz <konz@Konzs-MacBook-Air.local>
2023-10-05 10:55:41 +0700

    create routes and route for path to file that created

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
     </div>
   )
 }
