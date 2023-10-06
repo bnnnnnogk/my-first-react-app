17b42a415d1ce5aab770e7e4a7b40c2edf112939
Konz <konz@Konzs-MacBook-Air.local>
2023-10-06 11:36:35 +0700

    logic login on authprovider

--- src/providers/AuthProvider.tsx
+++ src/providers/AuthProvider.tsx
@@ -1,4 +1,6 @@
-import { ReactNode, createContext, useContext, useState } from 'react'
+import { ReactNode, createContext, useContext } from 'react'
+import { CredentialDTO, LoginDTO } from '../types/dto'
+import axios from 'axios'
 
 interface IAuthProviderProps {
   children: ReactNode
@@ -6,6 +8,7 @@ interface IAuthProviderProps {
 
 interface IAuthContextType {
   isLoggedIn: boolean
+  login: (username: string, password: string) => Promise<void>
 }
 
 const AuthContext = createContext<IAuthContextType | null>(null)
@@ -21,7 +24,21 @@ export const useAuth = () => {
 const AuthProvider = ({ children }: IAuthProviderProps) => {
   const [isLoggedIn, setIsLoggedin] = useState<boolean>(false)
 
-  return <AuthContext.Provider value={{ isLoggedIn }}>{children}</AuthContext.Provider>
+  const login = async (username: string, password: string) => {
+    const loginBody: LoginDTO = { username, password }
+
+    try {
+      const res = await axios.post<CredentialDTO>('https://api.learnhub.thanayut.in.th/auth/login', loginBody, {
+        headers: { 'Content-Type': 'application/json' },
+      })
+
+      console.log(res.data)
+    } catch (err) {
+      throw new Error('Invalid username or password')
+    }
+  }
+
+  return <AuthContext.Provider value={{ isLoggedIn, login }}>{children}</AuthContext.Provider>
 }
 
 export default AuthProvider
