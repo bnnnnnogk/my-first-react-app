5bc171907f70ab291c9204eefe5ecea5b5329e01
Konz <konz@Konzs-MacBook-Air.local>
2023-10-06 11:38:47 +0700

    add logic login on login page && use axios to help

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,23 @@
+import { FormEvent, useState } from 'react'
 import classes from './Login.module.css'
 
 const Login = () => {
+  const { login } = useAuth()
+  const [username, setUsername] = useState<string>('')
+  const [password, setPassword] = useState<string>('')
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault()
+
+    try {
+      await login(username, password)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
   return (
-    <form className={classes.loginForm}>
+    <form className={classes.loginForm} onSubmit={handleSubmit}>
       <label>Username:</label>
       <input type="text" />
 
